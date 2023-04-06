b6b5242e3a1582a42c7058720a542b896cc17515
Arindam Bose <adambose1990@gmail.com>
2023-04-06 11:59:07 -0400

    modal is centered

--- js/main.js
+++ js/main.js
@@ -227,7 +227,7 @@ class CommonDownloadForm extends HTMLElement {
             <div id="brochure-` + thisitem + `">
             <!-- Modal -->
             <div class="modal fade" id="popupModal-` + thisitem + `" tabindex="-1" role="dialog">
-                <div class="modal-dialog" role="document">
+                <div class="modal-dialog modal-dialog-centered" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
                             <button type="button" class="close" data-dismiss="modal">×</button>
